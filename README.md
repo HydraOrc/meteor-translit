69eea56a83f5cbbb81765586cd6add64c6ff75a4
hydra <hydra0@bigmir.net>
2015-09-15 15:27:03 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# meteor-translit
+URL-safe transliteration for Meteor.
+
+## Usage
+
+```
+translit('Школа управління продажем'); // Shkola_upravlinnya_prodazhem
+```
---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  name: 'hydraorc:translit',
+  version: '1.0.5',
+  // Brief, one-line summary of the package.
+  summary: 'URL-safe transliteration for Meteor.',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/hydraorc/meteor-translit.git',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1');
+  api.addFiles('translit.js');
+  api.export('translit');
+});
